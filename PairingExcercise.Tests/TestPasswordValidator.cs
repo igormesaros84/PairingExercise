4cfb4aac7109c47fe66cc145b238d299adc453c8
Igor Meszaros <igor.mesaros@bighand.com>
2023-01-27 08:57:32 +0000

    .net 6 upgrade

---
+++ PairingExcercise.Tests/TestPasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PairingExerciseTests
+{
+    [TestFixture]
+    public class TestPasswordValidator
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Validate_Should()
+        {
+        }
+    }
+}
---
+++ PairingExcercise/PasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PairingExercise
+{
+    public class PasswordValidator : IPasswordValidator
+    {
+        public int MinimumLength => throw new NotImplementedException();
+
+        public bool MixedCaseRequired => throw new NotImplementedException();
+
+        public bool NumericCharacterRequired => throw new NotImplementedException();
+
+        public ValidationResult Validate(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
